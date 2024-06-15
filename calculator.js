080088b32ce7f3922146ae587febe89ee59d067c
Isha Patel <143743208+ishi1518@users.noreply.github.com>
2024-06-16 01:18:48 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,9 @@
+ export let sumData=(a,b)=>{
+return a+b;
+}
+export let minDataa=(a,b) =>{
+    return a-b;
+}
+
+let cName="what";
+//export{sumData,minDataa}
---
+++ index.js
@@ -0,0 +1,5 @@
+import {sumData as sum,minDataa,cName} from "./calculator.js";
+
+console.log(sum(30,20))
+console.log(minDataa(30,20))
+console.log(cName)
